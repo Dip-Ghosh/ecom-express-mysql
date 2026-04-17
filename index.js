7bae967ab326d18f3849ef1e8fccef3ee13eff7a
Dip Ghosh <dipnsucse@gmail.com>
2026-04-17 20:26:32 +0600

    delete in mysql

--- index.js
+++ index.js
@@ -16,22 +16,53 @@ pool.getConnection((err, connection) => {
         console.log(err.sqlMessage || err.message);
     } else {
 
-        //select all
-        pool.query('select * from products',
-            (err, data) => {
-                if (err) console.log(err);
-                else console.log(data);
-            })
-
+        let lastInsertedId = null;
         //insert an item and fetch that item info too
         pool.query("Insert into products(name, price) values ('mike', 10)", (err, rsh) => {
             if (err) {
                 console.log(err.message);
             } else {
-                pool.query('select * from products where id = ?', [rsh.insertId],
+                console.log(lastInsertedId);
+                lastInsertedId =rsh.insertId;
+                    pool.query('select * from products where id = ?', [lastInsertedId],
                     (err, data) => {
-                        if (err) console.log(err);
-                        else console.log(data);
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+
+                            console.log('First Insertion----', data);
+                        }
+                    })
+            }
+        })
+
+        console.log(lastInsertedId);
+        //update a record
+        pool.query("update products set price= ? where id=?",[300, lastInsertedId],(err, rsh) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                console.log('Updated value ------', rsh);
+            }
+        })
+
+        //delete a recrod
+        pool.query("Delete from products where id=?",[lastInsertedId], (err, rsh) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                //select all
+                pool.query('select * from products',
+                    (err, data) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                        else {
+                            console.log('All Products-------', data);
+                        }
                     })
             }
         })
