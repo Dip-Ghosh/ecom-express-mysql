4f23dcbc24b02c060aebfb40b230acb2828f9561
Dip Ghosh <dipnsucse@gmail.com>
2026-04-17 20:42:03 +0600

    get api creation

--- index.js
+++ index.js
@@ -11,64 +11,75 @@ const pool = mysql.createPool({
     multipleStatements: true
 })
 
-pool.getConnection((err, connection) => {
-    if (err) {
-        console.log(err.sqlMessage || err.message);
-    } else {
+// pool.getConnection((err, connection) => {
+//     if (err) {
+//         console.log(err.sqlMessage || err.message);
+//     } else {
+//
+//         let lastInsertedId = null;
+//         //insert an item and fetch that item info too
+//         pool.execute("Insert into products(name, price) values ('mike', 10)", (err, rsh) => {
+//             if (err) {
+//                 console.log(err.message);
+//             } else {
+//                 console.log(lastInsertedId);
+//                 lastInsertedId =rsh.insertId;
+//                     pool.execute('select * from products where id = ?', [lastInsertedId],
+//                     (err, data) => {
+//                         if (err) {
+//                             console.log(err);
+//                         }
+//                         else {
+//
+//                             console.log('First Insertion----', data);
+//                         }
+//                     })
+//             }
+//         })
+//
+//         console.log(lastInsertedId);
+//         //update a record
+//         pool.execute("update products set price= ? where id=?",[300, lastInsertedId],(err, rsh) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 console.log('Updated value ------', rsh);
+//             }
+//         })
+//
+//         //delete a recrod
+//         pool.execute("Delete from products where id=?",[lastInsertedId], (err, rsh) => {
+//             if (err) {
+//                 console.log(err);
+//             }
+//             else {
+//                 //select all
+//                 pool.execute('select * from products',
+//                     (err, data) => {
+//                         if (err) {
+//                             console.log(err);
+//                         }
+//                         else {
+//                             console.log('All Products-------', data);
+//                         }
+//                     })
+//             }
+//         })
+//
+//         console.log("Connection established");
+//     }
+// })
 
-        let lastInsertedId = null;
-        //insert an item and fetch that item info too
-        pool.execute("Insert into products(name, price) values ('mike', 10)", (err, rsh) => {
+
+app.get('/getData', (req, res) => {
+    pool.execute('select * from products', (err, data) => {
             if (err) {
-                console.log(err.message);
+               res.status(500).send(err)
             } else {
-                console.log(lastInsertedId);
-                lastInsertedId =rsh.insertId;
-                    pool.execute('select * from products where id = ?', [lastInsertedId],
-                    (err, data) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-
-                            console.log('First Insertion----', data);
-                        }
-                    })
+                res.status(200).send(data);
             }
         })
-
-        console.log(lastInsertedId);
-        //update a record
-        pool.execute("update products set price= ? where id=?",[300, lastInsertedId],(err, rsh) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                console.log('Updated value ------', rsh);
-            }
-        })
-
-        //delete a recrod
-        pool.execute("Delete from products where id=?",[lastInsertedId], (err, rsh) => {
-            if (err) {
-                console.log(err);
-            }
-            else {
-                //select all
-                pool.execute('select * from products',
-                    (err, data) => {
-                        if (err) {
-                            console.log(err);
-                        }
-                        else {
-                            console.log('All Products-------', data);
-                        }
-                    })
-            }
-        })
-
-        console.log("Connection established");
-    }
 })
 
 app.listen(3000, () => {
