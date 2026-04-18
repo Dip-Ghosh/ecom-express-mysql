1b7a66744c9cb4a7176c20014be151a247f4081e
Dip Ghosh <dipnsucse@gmail.com>
2026-04-18 14:13:39 +0600

    refactoring

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const router = require('./routes/products')
 const app = express();
 
 app.use('/', router);
+app.use(bodyParser.urlencoded());
 
 app.listen(3000, () => {
     console.log(`Server is running...`)
